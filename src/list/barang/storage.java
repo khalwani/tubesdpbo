4152529c708bc47a4a3424504571f028ae4c9992
Khalwani <naufalutbk6@gmail.com>
2025-05-26 13:44:25 +0700

    initial commit

---
+++ src/list/barang/storage.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package list.barang;
+
+/**
+ *
+ * @author naufal kafabih kh
+ */
+import java.util.HashMap;
+
+public class storage {
+    // Menyimpan username dan password
+    public static HashMap<String, String> akunMap = new HashMap<>();
+    public static HashMap<String, String> namaJenisBarang = new HashMap<>();
+    public static HashMap<String, String> jumlahBarangNoKamar = new HashMap<>();
+    
+}
+
+
